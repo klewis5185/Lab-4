6dcebebb59d39ba2460cf2b1d8cc9cb4b3772939
Kyle Lewis <klewis5185@gmail.com>
2018-07-23 20:34:17 -0700

    added main method

--- src/edu/pcc/cis/Lab4/Lab4.java
+++ src/edu/pcc/cis/Lab4/Lab4.java
@@ -4,5 +4,7 @@ package edu.pcc.cis.Lab4;
  * Created by klewis5185 on 7/23/18
  */
 public class Lab4 {
-    System.out.println("Hello Everybody!")
+    public static void main(String[] args) {
+        System.out.println("Hello Professor!");
+    }
 }
