be775060ab40a3cdc63745df7a88f2f2472d16b0
Kyle Lewis <klewis5185@gmail.com>
2018-07-24 18:10:59 -0700

    print date

--- src/edu/pcc/cis/Lab4/Lab4.java
+++ src/edu/pcc/cis/Lab4/Lab4.java
@@ -1,5 +1,5 @@
 package edu.pcc.cis.Lab4;
-
+import java.util.Date;
 /**
  * Created by klewis5185 on 7/23/18
  */
@@ -7,5 +7,6 @@ public class Lab4 {
     public static void main(String[] args) {
         System.out.println("Hello Professor!");
         System.out.println("I hope this is all working correctly.");
+        System.out.println(new Date());
     }
 }
